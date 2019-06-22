3130749c935f3adbd8c9e12fbe3d34b00c74a1c4
darksworm <9987548+darksworm@users.noreply.github.com>
2019-06-22 16:43:12 +0300

    basic working keypress filter

--- src/input/handler/filters/KeyFilteringInputHandler.cpp
+++ src/input/handler/filters/KeyFilteringInputHandler.cpp
@@ -2,17 +2,29 @@
 #include "KeyFilteringInputHandler.h"
 #include "../../../lib/keycode/keycode.h"
 
+std::string linux_rawname_to_yaml_name(std::string linux_rawname) {
+    if (linux_rawname.rfind("LEFT", 0) == 0) {
+        linux_rawname = linux_rawname.substr(4, std::string::npos);
+    } else if (linux_rawname.rfind("RIGHT", 0) == 0) {
+        linux_rawname = linux_rawname.substr(5, std::string::npos);
+    }
+
+    if (linux_rawname == "CTRL") {
+        linux_rawname = "CONTROL";
+    }
+
+    return linux_rawname;
+}
+
 std::string KeyFilteringInputHandler::getFilterText() {
     std::string str;
 
-    for(auto &it:buffer)
-    {
-        if(!str.empty())
-        {
+    for (auto &it:buffer) {
+        if (!str.empty()) {
             str.append(" ");
         }
 
-        str.append(keycode_linux_rawname(it));
+        str.append(linux_rawname_to_yaml_name(keycode_linux_rawname(it)));
     }
 
     return str;
@@ -28,15 +40,16 @@ std::function<bool(Hotkey *)> KeyFilteringInputHandler::getFilter() {
 
     return std::function<bool(Hotkey *hotkey)>(
             [b](Hotkey *hotkey) {
-//                for(auto &it : b) {
-//                    auto keyStr = keycode_linux_rawname(it);
-//
-//                    // TODO: we need a translator from key name(from config file) to linux key name
-//                    if(hotkey->getKeyCodes()->find(keyStr) != hotkey->getKeyCodes()->end())
-//                    {
-//                        return false;
-//                    }
-//                }
+                for (auto &it : b) {
+                    std::string keyStr = keycode_linux_rawname(it);
+
+                    keyStr = linux_rawname_to_yaml_name(keyStr);
+
+                    // TODO: we need a translator from key name(from config file) to linux key name
+                    if (hotkey->getKeyCodes()->find(keyStr) == hotkey->getKeyCodes()->end()) {
+                        return false;
+                    }
+                }
 
                 return true;
             }
@@ -44,10 +57,10 @@ std::function<bool(Hotkey *)> KeyFilteringInputHandler::getFilter() {
 }
 
 bool KeyFilteringInputHandler::shouldAddToBuffer(unsigned keyPress) {
-    if(keyPress == KEY_ESC)
-    {
+    if (keyPress == KEY_ESC) {
         return false;
     }
 
     return true;
 }
+
